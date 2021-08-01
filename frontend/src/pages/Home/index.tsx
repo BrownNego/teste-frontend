669904ce923887e97eec23b2268447caf33d7338
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2021-08-01 16:15:31 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,20 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Fluid jumbotron</h1>
+                    <p className="lead">Este é um jumbotron modificado que ocupa todo o espaço horizontal de seu elemento pai.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard"> Acessar o dashboard</Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
